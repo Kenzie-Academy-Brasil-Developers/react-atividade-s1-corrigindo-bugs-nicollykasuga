ab4563191fa162985f287b8169c39181331b8c74
Nicky <nicollyporfirio12@gmail.com>
2021-11-08 09:08:16 -0300

    Fixed

--- src/components/cards-list/index.jsx
+++ src/components/cards-list/index.jsx
@@ -1,70 +1,72 @@
 import { useState } from "react";
-import { Card } from "../card";
+import Card from "../card";
 import "./style.css";
 
 const CardsList = ({ cardsList }) => {
   const [suitFilter, setSuitFilter] = useState(false);
 
-  const handleSuitFilter = (e) => {
-    if (e.target.id = suitFilter) {
-      suitFilter = true;
+  const handleSuitFilter = e => {
+    if (e.target.id === suitFilter) {
+      setSuitFilter(false);
       e.target.checked = false;
     } else {
-      suitFilter = e.target.id
+      setSuitFilter(e.target.id);
     }
   };
 
   return (
-    <div className="filter-container">
-      <p>Filtrar por naipe</p>
-      <div>
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="SPADES"
-          name="suit"
-        />
-        <label for="SPADES">Espadas</label>
+    <>
+      <div className="filter-container">
+        <p>Filtrar por naipe</p>
+        <div>
+          <input
+            onClick={handleSuitFilter}
+            type="radio"
+            id="SPADES"
+            name="suit"
+          />
+          <label for="SPADES">Espadas</label>
 
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="HEARTS"
-          name="suit"
-        />
-        <label for="HEARTS">Copas</label>
+          <input
+            onClick={handleSuitFilter}
+            type="radio"
+            id="HEARTS"
+            name="suit"
+          />
+          <label for="HEARTS">Copas</label>
 
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="CLUBS"
-          name="suit"
-        />
-        <label for="CLUBS">Paus</label>
+          <input
+            onClick={handleSuitFilter}
+            type="radio"
+            id="CLUBS"
+            name="suit"
+          />
+          <label for="CLUBS">Paus</label>
 
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="DIAMONDS"
-          name="suit"
-        />
-        <label for="DIAMONDS">Ouros</label>
+          <input
+            onClick={handleSuitFilter}
+            type="radio"
+            id="DIAMONDS"
+            name="suit"
+          />
+          <label for="DIAMONDS">Ouros</label>
+        </div>
       </div>
-    </div>
-    <div>
-
-      {!suitFilter &&
-        cardsList.map((actual, index) => {
-          return <Card card={actual} key={index} />;
-        })}
-
-      {suitFilter &&
-        cardsList
-          .filter((actual) => actual.suit === suitFilter)
-          .map((actual, index) => {
-            <Card card={actual} key={index} />;
+      <div>
+        {!suitFilter &&
+          cardsList.map((actual, index) => {
+            return <Card card={actual} key={index} />;
           })}
-    </div>
+
+        {suitFilter &&
+          cardsList
+            .filter(actual => actual.suit === suitFilter)
+            .map((actual, index) => {
+              return <Card card={actual} key={index} />;
+            })}
+      </div>
+    </>
   );
 };
 
+export default CardsList;
